d99b65d22a58086088d2a624a22195c0f7bd13e5
Maksym Drachenko <maks7d@gmail.com>
2025-03-09 18:20:11 +0500

    add 4 function

---
+++ js/lesson-5.js
@@ -0,0 +1,10 @@
+// console.log(localStorage);
+
+// const number = 10;
+// localStorage.setItem('test', number);
+
+// console.log(JSON.parse(localStorage.getItem('test')));
+
+// localStorage.setItem('test2', JSON.stringify([1, 2, 3]));
+
+// console.log(JSON.parse(localStorage.getItem('test2')));
